977ab7f33d49df2b78320e4ebe1d062f7e97e822
Zealll <eriznis@gmail.com>
2019-04-15 19:42:55 -0400

    finished MVP

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,11 +6,37 @@ const enhancer = require('./enhancer.js');
     it('Reapair = 100', () => {
         const item = {
             name: 'car',
-            durability: 50
+            durability: 50,
+            enhancement: 17
         }
     
         expect(enhancer.repair(item.durability)).toBe(100)
     })
 // })
+
+// describe('success', () => {
+    it('success', () => {
+        const item = {
+            name: 'car',
+            durability: 90,
+            enhancement: 16
+        }
+    
+        expect(enhancer.succeed(item.enhancement)).toBe(17)
+        expect(enhancer.succeed(item.enhancement + 10)).toBe(20)
+    })
+// })
+
+
+it('fail', () => {
+    const item = {
+        name: 'car',
+        durability: 90,
+        enhancement: 10
+    }
+
+    expect(enhancer.fail(item)).toBe(85)
+})
+
 
 
