9cda58fef2f68ccaebf7672bc1dfff37f0ebb7ba
Zealll <eriznis@gmail.com>
2019-04-15 18:46:24 -0400

    repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+// describe('func', () => {
+    it('Reapair = 100', () => {
+        const item = {
+            name: 'car',
+            durability: 50
+        }
+    
+        expect(enhancer.repair(item.durability)).toBe(100)
+    })
+// })
+
+
